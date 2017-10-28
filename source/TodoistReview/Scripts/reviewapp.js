59e3b76b16458cce7c0c3a373f4b80894ee7770e
Taurit <git@taurit.pl>
2017-10-28 14:44:10 +0200

    Fix JS references old version of jQuery

--- source/TodoistReview/Scripts/reviewapp.js
+++ source/TodoistReview/Scripts/reviewapp.js
@@ -1,5 +1,5 @@
 ﻿/// <reference path="knockout-3.4.2.debug.js" />
-/// <reference path="jquery-3.1.1.intellisense.js" />
+/// <reference path="jquery-3.2.1.intellisense.js" />
 
 $(document).ready(function () {
     "use strict";
