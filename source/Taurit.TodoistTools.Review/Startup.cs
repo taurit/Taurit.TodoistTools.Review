ee5d50566a18b0b982898e7990ca81e979db175c
Paweł Bulwan <github@taurit.pl>
2022-03-20 19:04:38 +0100

    Remove AppInsights integration

--- source/Taurit.TodoistTools.Review/Startup.cs
+++ source/Taurit.TodoistTools.Review/Startup.cs
@@ -8,7 +8,6 @@ public class Startup
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-        services.AddApplicationInsightsTelemetry();
         services.Configure<CookiePolicyOptions>(options =>
         {
             // This lambda determines whether user consent for non-essential cookies is needed for a given request.
